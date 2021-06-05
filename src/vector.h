016fea116867f4b63b040c0b4f096822cb3a4ddb
InvidusS <47109249+InvidusS@users.noreply.github.com>
2021-06-06 00:56:05 +0200

    Update vector.h

--- src/vector.h
+++ src/vector.h
@@ -35,67 +35,107 @@ public:
     T& operator[](int it) const{
         return vector[it];
     }
-    T& frontOfVector() const {
-        return vector[0];
+    T& frontElement() const;
+    T& backElement() const;
+    bool isEmpty() const;
+    int sizeOfVector() const;
+    int capacityOfVector() const;
+    void clearVector();
+    void insertElement(int, T);
+    void ereaseElement(int);
+    void pushBack(T);
+    void popBack();
+    bool search(T);
+};
+
+template <typename T>
+T& Vector<T>::frontElement() const {
+    return vector[0];
+}
+
+template <typename T>
+T& Vector<T>::backElement() const {
+    return vector[size - 1];
+}
+
+template <typename T>
+bool Vector<T>::isEmpty() const {
+    return size == 0;
+}
+
+template <typename T>
+int Vector<T>::sizeOfVector() const {
+    return size;
+}
+
+template <typename T>
+int Vector<T>::capacityOfVector() const {
+    return capacity;
+}
+
+template <typename T>
+void Vector<T>::clearVector(){
+    size = 0;
+}
+
+template <typename T>
+void Vector<T>::insertElement(int it, T value){
+    if(it < 0 || it >= size){
+        throw std::out_of_range("Error: it out of range");
+    } else if(size != capacity){
+        for(int i = size - 1; i >= it; i--)
+            vector[i + 1] = vector[i];
+        vector[it] = value;
+        size++;
     }
-    T& backOfVector() const {
-        return vector[size - 1];
+}
+
+template <typename T>
+void Vector<T>::ereaseElement(int it){
+    if(it < 0 || it >= size){
+        throw std::out_of_range("Error: it out of range");
+    } else {
+        for(int i = it; i < size - 1; i++)
+            vector[i] = vector[i + 1];
     }
-    bool isEmpty() const {
-        return size == 0;
-    }
-    int sizeOfVector() const {
-        return size;
-    }
-    int capacityOfVector() const {
-        return capacity;
-    }
-    void insertIntoVector(int it, T value){
-        if(it < 0 || it >= size){
-            throw std::out_of_range("Error: it out of range");
-        } else if(size != capacity){
-            for(int i = size - 1; i >= it; i--)
-                vector[i + 1] = vector[i];
-            vector[it] = value;
-            size++;
+    size--;
+}
+
+template <typename T>
+void Vector<T>::pushBack(T value) {
+    if(size < capacity){
+        vector[size] = value;
+        size++;
+    } else {
+        capacity += 5;
+        T* newVector = new T[capacity];
+
+        for(int i = 0; i < size; i ++){
+            newVector[i] = vector[i];
         }
+
+        newVector[size] = value;
+        size++;
+
+        delete[] vector;
+        vector = newVector;
     }
-    void ereaseElementOfVector(int it){
-        if(it < 0 || it >= size){
-            throw std::out_of_range("Error: it out of range");
-        } else {
-            for(int i = it; i < size - 1; i++)
-                vector[i] = vector[i + 1];
-        }
+}
+
+template <typename T>
+void Vector<T>::popBack(){
+    if(size == 0){
+        throw std::out_of_range("Error: vector is already empty");
+    } else {
         size--;
     }
-    void clearVector(){
-        size = 0;
-    }
-    void pushBack(T value) {
-        if(size < capacity){
-            vector[size] = value;
-            size++;
-        } else {
-            capacity += 5;
-            T* newVector = new T[capacity];
+}
 
-            for(int i = 0; i < size; i ++){
-                newVector[i] = vector[i];
-            }
-
-            newVector[size] = value;
-            size++;
-
-            delete[] vector;
-            vector = newVector;
-        }
+template <typename T>
+bool Vector<T>::search(T value){
+    for(int i = 0; i < size - 1; i++){
+        if(vector[i] == value)
+            return true;
     }
-    void popBack(){
-        if(size == 0){
-            throw std::out_of_range("Error: vector is already empty");
-        } else {
-            size--;
-        }
-    }
-};
+    return false;
+}
